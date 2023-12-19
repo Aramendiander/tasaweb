d36e3037cc3651ca4634b3649ef593585153343d
albinramos <dramosh79@gmail.com>
2023-12-19 13:04:26 +0100

    css arreglado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Form from '../components/form/Form.jsx';
+import './home.css';
 
 const Home = () => {
 
 
     return (
-        <div>
+        <div className='home-container'>
             <main className='home-main'>
                 <section>
-                    <h1>¿Vas a alquilar?<br />¡Descubre cuanto vale tu inmueble!</h1>
+                    <h1 className='titulo-home'>¿Vas a alquilar?<br />¡Descubre cuanto<br>
+                    </br> vale tu inmueble!</h1>
                 </section>
                 <div className='home-container'>
                     <div>
-                        <h1>Home</h1>
                         <Outlet />
                         <Form />
                     </div>
